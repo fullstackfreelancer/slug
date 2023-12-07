fb45ad829e9f253dd3fc3e0977a518303e106ebb
koehlersimon <simonkohler@192.168.2.1>
2023-12-06 23:22:34 -0500

    Remove comment

--- ext_localconf.php
+++ ext_localconf.php
@@ -7,5 +7,3 @@
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScriptSetup(
     '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:slug/Configuration/TypoScript/setup.typoscript">'
 );
-
-//$GLOBALS['TYPO3_CONF_VARS']['BE']['stylesheets']['slug'] = 'EXT:slug/Resources/Public/Css/Backend.css';
