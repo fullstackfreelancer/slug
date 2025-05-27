03cef60a2f2939681dd72c9190a068ee720aecc5
koehlersimon <simonkohler@192.168.2.1>
2025-05-26 19:31:13 -0500

    Remove unused classes

--- Classes/Domain/Repository/Typo3PageRepository.php
+++
@@ -1,6 +0,0 @@
-<?php
-namespace SIMONKOEHLER\Slug\Domain\Repository;
-
-class Typo3PageRepository extends \TYPO3\CMS\Core\Domain\Repository\PageRepository {
-
-}
--- Classes/Domain/Repository/Typo3TreeRepository.php
+++
@@ -1,6 +0,0 @@
-<?php
-namespace SIMONKOEHLER\Slug\Domain\Repository;
-
-class Typo3TreeRepository extends \TYPO3\CMS\Backend\Tree\Repository\PageTreeRepository{
-
-}
