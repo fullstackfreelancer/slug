ccb62c2d45df31cfd30620fc41cdbc8cb66ca43e
koehlersimon <simonkohler@192.168.2.1>
2025-05-27 20:12:08 -0500

    Remove console statements

--- Resources/Public/JavaScript/PageController.js
+++ Resources/Public/JavaScript/PageController.js
@@ -1,9 +1,7 @@
 import AjaxRequest from "@typo3/core/ajax/ajax-request.js";
-console.log('PageController.js works!')
 
 /*
 let request = new AjaxRequest(TYPO3.settings.ajaxUrls['slug_info']);
-console.log('HELL YEAH')
 const json = {uid: 1};
 let promise = request.post(json, {
   headers: {
@@ -14,7 +12,5 @@ let promise = request.post(json, {
 promise.then(async function (response) {
     const responseText = await response.resolve();
     document.getElementById('slug-preview').innerHTML = responseText
-    console.log(responseText);
-    //console.log(TYPO3.settings.ajaxUrls)
 });
 */
